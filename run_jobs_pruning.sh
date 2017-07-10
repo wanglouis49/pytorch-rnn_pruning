f8e16fc6b454fc36e3a92aeb186c4583d99c0977
Luyu Wang <wanglouis49@gmail.com>
2017-07-09 20:42:29 -0400

    more layers

--- run_jobs_pruning.sh
+++ run_jobs_pruning.sh
@@ -1,54 +1,121 @@
-python prunning.py gru 10
-python prunning.py gru 15
-python prunning.py gru 20
-python prunning.py gru 25
-python prunning.py gru 30
-python prunning.py gru 35
-python prunning.py gru 40
-python prunning.py gru 45
-python prunning.py gru 50
-python prunning.py gru 55
-python prunning.py gru 60
-python prunning.py gru 65
-python prunning.py gru 70
-python prunning.py gru 75
-python prunning.py gru 80
-python prunning.py gru 85
-python prunning.py gru 90
-python prunning.py gru 95
-python prunning.py lstm 10
-python prunning.py lstm 15
-python prunning.py lstm 20
-python prunning.py lstm 25
-python prunning.py lstm 30
-python prunning.py lstm 35
-python prunning.py lstm 40
-python prunning.py lstm 45
-python prunning.py lstm 50
-python prunning.py lstm 55
-python prunning.py lstm 60
-python prunning.py lstm 65
-python prunning.py lstm 70
-python prunning.py lstm 75
-python prunning.py lstm 80
-python prunning.py lstm 85
-python prunning.py lstm 90
-python prunning.py lstm 95
-python prunning.py rnn 10
-python prunning.py rnn 15
-python prunning.py rnn 20
-python prunning.py rnn 25
-python prunning.py rnn 30
-python prunning.py rnn 35
-python prunning.py rnn 40
-python prunning.py rnn 45
-python prunning.py rnn 50
-python prunning.py rnn 55
-python prunning.py rnn 60
-python prunning.py rnn 65
-python prunning.py rnn 70
-python prunning.py rnn 75
-python prunning.py rnn 80
-python prunning.py rnn 85
-python prunning.py rnn 90
-python prunning.py rnn 95
+python prunning.py gru 10 2
+python prunning.py gru 15 2
+python prunning.py gru 20 2
+python prunning.py gru 25 2
+python prunning.py gru 30 2
+python prunning.py gru 35 2
+python prunning.py gru 40 2
+python prunning.py gru 45 2
+python prunning.py gru 50 2
+python prunning.py gru 55 2
+python prunning.py gru 60 2
+python prunning.py gru 65 2
+python prunning.py gru 70 2
+python prunning.py gru 75 2
+python prunning.py gru 80 2
+python prunning.py gru 85 2
+python prunning.py gru 90 2
+python prunning.py gru 95 2
+python prunning.py gru 97 2
+python prunning.py gru 99 2
+python prunning.py lstm 10 2
+python prunning.py lstm 15 2
+python prunning.py lstm 20 2
+python prunning.py lstm 25 2
+python prunning.py lstm 30 2
+python prunning.py lstm 35 2
+python prunning.py lstm 40 2
+python prunning.py lstm 45 2
+python prunning.py lstm 50 2
+python prunning.py lstm 55 2
+python prunning.py lstm 60 2
+python prunning.py lstm 65 2
+python prunning.py lstm 70 2
+python prunning.py lstm 75 2
+python prunning.py lstm 80 2
+python prunning.py lstm 85 2
+python prunning.py lstm 90 2
+python prunning.py lstm 95 2
+python prunning.py lstm 97 2
+python prunning.py lstm 99 2
+python prunning.py rnn 10 2
+python prunning.py rnn 15 2
+python prunning.py rnn 20 2
+python prunning.py rnn 25 2
+python prunning.py rnn 30 2
+python prunning.py rnn 35 2
+python prunning.py rnn 40 2
+python prunning.py rnn 45 2
+python prunning.py rnn 50 2
+python prunning.py rnn 55 2
+python prunning.py rnn 60 2
+python prunning.py rnn 65 2
+python prunning.py rnn 70 2
+python prunning.py rnn 75 2
+python prunning.py rnn 80 2
+python prunning.py rnn 85 2
+python prunning.py rnn 90 2
+python prunning.py rnn 95 2
+python prunning.py rnn 97 2
+python prunning.py rnn 99 2
+python prunning.py gru 10 3
+python prunning.py gru 15 3
+python prunning.py gru 20 3
+python prunning.py gru 25 3
+python prunning.py gru 30 3
+python prunning.py gru 35 3
+python prunning.py gru 40 3
+python prunning.py gru 45 3
+python prunning.py gru 50 3
+python prunning.py gru 55 3
+python prunning.py gru 60 3
+python prunning.py gru 65 3
+python prunning.py gru 70 3
+python prunning.py gru 75 3
+python prunning.py gru 80 3
+python prunning.py gru 85 3
+python prunning.py gru 90 3
+python prunning.py gru 95 3
+python prunning.py gru 97 3
+python prunning.py gru 99 3
+python prunning.py lstm 10 3
+python prunning.py lstm 15 3
+python prunning.py lstm 20 3
+python prunning.py lstm 25 3
+python prunning.py lstm 30 3
+python prunning.py lstm 35 3
+python prunning.py lstm 40 3
+python prunning.py lstm 45 3
+python prunning.py lstm 50 3
+python prunning.py lstm 55 3
+python prunning.py lstm 60 3
+python prunning.py lstm 65 3
+python prunning.py lstm 70 3
+python prunning.py lstm 75 3
+python prunning.py lstm 80 3
+python prunning.py lstm 85 3
+python prunning.py lstm 90 3
+python prunning.py lstm 95 3
+python prunning.py lstm 97 3
+python prunning.py lstm 99 3
+python prunning.py rnn 10 3
+python prunning.py rnn 15 3
+python prunning.py rnn 20 3
+python prunning.py rnn 25 3
+python prunning.py rnn 30 3
+python prunning.py rnn 35 3
+python prunning.py rnn 40 3
+python prunning.py rnn 45 3
+python prunning.py rnn 50 3
+python prunning.py rnn 55 3
+python prunning.py rnn 60 3
+python prunning.py rnn 65 3
+python prunning.py rnn 70 3
+python prunning.py rnn 75 3
+python prunning.py rnn 80 3
+python prunning.py rnn 85 3
+python prunning.py rnn 90 3
+python prunning.py rnn 95 3
+python prunning.py rnn 97 3
+python prunning.py rnn 99 3
+
--- run_jobs_test.sh
+++ run_jobs_test.sh
@@ -1,5 +1,6 @@
-python cnn_weights.py 1 3 1
-python cnn_weights.py 2 3 1
-python cnn_weights.py 3 3 1
-python cnn_weights.py 4 3 1
-python cnn_weights.py 5 3 1
+python prunning.py lstm 97 1
+python prunning.py lstm 99 1
+python prunning.py rnn 97 1
+python prunning.py rnn 99 1
+python prunning.py gru 97 1
+python prunning.py gru 99 1
