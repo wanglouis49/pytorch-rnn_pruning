6d808c0083be051c285afa10cbd45bacb8c601ac
Luyu Wang <wanglouis49@gmail.com>
2017-07-12 19:41:52 -0400

    checkin

--- plot_results.py
+++ plot_results.py
@@ -3,44 +3,97 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 
 model = sys.argv[1]
+layers = sys.argv[2]
 
-pruning_percentage = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 97, 99]
+if layers == '1':
+	f_name = ''
+else:
+	f_name = '_'+layers
 
-# # accuracy vs pruning percentage
-# # with open('model/'+model+'_conv.pkl','r') as f:
-# with open('model/'+model+'_2_conv.pkl','r') as f:
-# # with open('model/'+model+'_conv.pkl','r') as f:
+# pruning_percentage = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 97, 99]
+pruning_percentage = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 97]
+
+
+
+# Accuracy vs pruning percentage
+fig, axes = plt.subplots(nrows=3, ncols=1)
+models = ['rnn', 'lstm', 'gru']
+for mid, model in enumerate(models):
+	# loss & accuracy vs pruning percentage
+	with open('model/'+model+f_name+'_conv.pkl','r') as f:
+		data = pkl.load(f)
+		accuracy_original = data['accuracies'][-1]
+		loss_original = data['losses'][-1]
+
+	accuracies_prunning = []; losses_prunning = []
+	accuracies_pruned = []; losses_pruned = []
+	for i in pruning_percentage:
+		with open('model/'+model+'_'+str(i)+f_name+'_retrained_conv.pkl','r') as f:
+			data = pkl.load(f)
+			accuracies_prunning.append(data['accuracies'][0])
+			losses_prunning.append(data['losses'][0])
+			accuracies_pruned.append(data['accuracies'][-1])
+			losses_pruned.append(data['losses'][-1])
+
+
+	plt.subplot(3,1,mid+1)
+	plt.plot(pruning_percentage, accuracies_pruned)
+	# plt.plot(pruning_percentage, accuracies_prunning)
+	plt.plot((pruning_percentage[0], pruning_percentage[-1]), (accuracy_original, accuracy_original), 'k--')
+	plt.legend(['w/ retraining','w/o retrianing','no pruning'])
+	plt.xlabel('percentage pruned (%)')
+	plt.ylabel('test accuracy (%)')
+	plt.title(model+' '+layers+' layers')
+	plt.grid(True)
+fig.tight_layout()
+plt.show()
+
+
+# loss & accuracy vs pruning percentage
+# with open('model/'+model+f_name+'_conv.pkl','r') as f:
 # 	data = pkl.load(f)
 # 	accuracy_original = data['accuracies'][-1]
 # 	loss_original = data['losses'][-1]
 
 # accuracies_prunning = []; losses_prunning = []
+# accuracies_pruned = []; losses_pruned = []
 # for i in pruning_percentage:
-# 	# with open('model/'+model+'_'+str(i)+'_retrained_conv.pkl','r') as f:
-# 	with open('model/'+model+'_'+str(i)+'_2_retrained_cipytonv.pkl','r') as f:
+# 	with open('model/'+model+'_'+str(i)+f_name+'_retrained_conv.pkl','r') as f:
 # 		data = pkl.load(f)
-# 		accuracies_prunning.append(data['accuracies'][-1])
-# 		losses_prunning.append(data['losses'][-1])
+# 		accuracies_prunning.append(data['accuracies'][0])
+# 		losses_prunning.append(data['losses'][0])
+# 		accuracies_pruned.append(data['accuracies'][-1])
+# 		losses_pruned.append(data['losses'][-1])
+
 # plt.subplot(2,1,1)
-# plt.plot(pruning_percentage, accuracies_prunning)
-# plt.plot((pruning_percentage[0], pruning_percentage[-1]), (accuracy_original, accuracy_original), 'k--')
-# plt.subplot(2,1,2)
+# plt.plot(pruning_percentage, losses_pruned)
 # plt.plot(pruning_percentage, losses_prunning)
 # plt.plot((pruning_percentage[0], pruning_percentage[-1]), (loss_original, loss_original), 'k--')
+# plt.legend(['w/ retraining','w/o retrianing','no pruning'])
+# plt.xlabel('percentage pruned (%)')
+# plt.ylabel('loss')
+# plt.title(model+' '+layers+' layers')
+# plt.grid(True)
+# plt.subplot(2,1,2)
+# plt.plot(pruning_percentage, accuracies_pruned)
+# plt.plot(pruning_percentage, accuracies_prunning)
+# plt.plot((pruning_percentage[0], pruning_percentage[-1]), (accuracy_original, accuracy_original), 'k--')
+# plt.legend(['w/ retraining','w/o retrianing','no pruning'])
+# plt.xlabel('percentage pruned (%)')
+# plt.ylabel('test accuracy (%)')
+# plt.grid(True)
 # plt.show()
 
 
 
 # plot convergence
-# with open('model/'+model+'_conv.pkl','r') as f:
-# with open('model/'+model+'_2_conv.pkl','r') as f:
+# with open('model/'+model+f_name+'_conv.pkl','r') as f:
 # 	data = pkl.load(f)
 # 	accuracies_original = data['accuracies']
 # 	losses_original = data['losses']
 
 # accuracies_prunning = []; losses_prunning = []
-# # with open('model/'+model+'_'+'95'+'_retrained_conv.pkl','r') as f:
-# with open('model/'+model+'_'+'95'+'_2_retrained_conv.pkl','r') as f:
+# with open('model/'+model+'_'+'95'+f_name+'_retrained_conv.pkl','r') as f:
 # 	data = pkl.load(f)
 # 	accuracies_prunning = data['accuracies']
 # 	losses_prunning = data['losses']
@@ -53,36 +106,36 @@ pruning_percentage = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80
 
 
 # plot weight distribution
-with open('model/'+model+'_'+'95'+'_2_retrained_conv.pkl','r') as f:
-	data = pkl.load(f)
+# with open('model/'+model+'_'+'95'+f_name+'_retrained_conv.pkl','r') as f:
+# 	data = pkl.load(f)
 
-w_original = data['w_original']
-w_pruned = data['w_pruned']
-w_retrained = data['w_retrained']
-pruned_inds_by_layer = data['pruned_inds_by_layer']
-losses = data['losses']
-accuracies = data['accuracies']
+# w_original = data['w_original']
+# w_pruned = data['w_pruned']
+# w_retrained = data['w_retrained']
+# pruned_inds_by_layer = data['pruned_inds_by_layer']
+# losses = data['losses']
+# accuracies = data['accuracies']
 
 
-plt.figure(0)
-plt.subplot(3,1,1)
-plt.hist(w_original[0].data.numpy().flatten(), bins=100)
-plt.subplot(3,1,2)
-plt.hist(w_original[1].data.numpy().flatten(), bins=100)
-plt.subplot(3,1,3)
-plt.hist(w_original[2].data.numpy().flatten(), bins=100)
-plt.figure(1)
-plt.subplot(3,1,1)
-plt.hist(w_pruned[0].data[1-pruned_inds_by_layer[0]].numpy(), bins=100)
-plt.subplot(3,1,2)
-plt.hist(w_pruned[1].data[1-pruned_inds_by_layer[1]].numpy(), bins=100)
-plt.subplot(3,1,3)
-plt.hist(w_pruned[2].data[1-pruned_inds_by_layer[2]].numpy(), bins=100)
-plt.figure(2)
-plt.subplot(3,1,1)
-plt.hist(w_retrained[0].data[1-pruned_inds_by_layer[0]].numpy(), bins=100)
-plt.subplot(3,1,2)
-plt.hist(w_retrained[1].data[1-pruned_inds_by_layer[1]].numpy(), bins=100)
-plt.subplot(3,1,3)
-plt.hist(w_retrained[2].data[1-pruned_inds_by_layer[2]].numpy(), bins=100)
-plt.show()
+# plt.figure(0)
+# plt.subplot(3,1,1)
+# plt.hist(w_original[0].data.numpy().flatten(), bins=100)
+# plt.subplot(3,1,2)
+# plt.hist(w_original[1].data.numpy().flatten(), bins=100)
+# plt.subplot(3,1,3)
+# plt.hist(w_original[2].data.numpy().flatten(), bins=100)
+# plt.figure(1)
+# plt.subplot(3,1,1)
+# plt.hist(w_pruned[0].data[1-pruned_inds_by_layer[0]].numpy(), bins=100)
+# plt.subplot(3,1,2)
+# plt.hist(w_pruned[1].data[1-pruned_inds_by_layer[1]].numpy(), bins=100)
+# plt.subplot(3,1,3)
+# plt.hist(w_pruned[2].data[1-pruned_inds_by_layer[2]].numpy(), bins=100)
+# plt.figure(2)
+# plt.subplot(3,1,1)
+# plt.hist(w_retrained[0].data[1-pruned_inds_by_layer[0]].numpy(), bins=100)
+# plt.subplot(3,1,2)
+# plt.hist(w_retrained[1].data[1-pruned_inds_by_layer[1]].numpy(), bins=100)
+# plt.subplot(3,1,3)
+# plt.hist(w_retrained[2].data[1-pruned_inds_by_layer[2]].numpy(), bins=100)
+# plt.show()
